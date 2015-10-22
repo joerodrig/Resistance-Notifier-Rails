da6afa833bd9ed88d5a6c81ad9716d7283ab315e
Joseph <joerodrig3@gmail.com>
2015-10-21 23:54:27 -0400

    adding games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :users
-
+  has_many :games
+  
   def generate_roles player_count
     role_count =
       case player_count
